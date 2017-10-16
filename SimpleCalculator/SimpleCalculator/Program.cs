06daae996dfdfc0f0a60837cb575c6d840bfae5d
Neous1 <Neous1@users.noreply.github.com>
2017-10-16 13:24:09 -0700

    if else

---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            if (input.Equals("Avetis"))
+            {
+                Console.WriteLine("Hello Admin!");
+            }
+            else if (input.Equals("Joe"))
+            {
+                Console.WriteLine("Hello Moderator!");
+            }
+            else 
+            {
+                Console.WriteLine("Hello Guest!");
+            }
+        }
+    }
+}

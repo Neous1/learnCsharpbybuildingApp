ded4c48e97c7efc357aa14746fe9f764174dc154
Neous1 <Neous1@users.noreply.github.com>
2017-10-17 02:44:33 -0700

    take our 2 input and perform operation

--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -10,8 +10,12 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            InputConverter inputConverter = new InputConverter();
-            CalculatorEngine calculatorEngine = new calculatorEngine();
+            InputConverter inputConverter = new InputConverter();//this class converts the users input
+            CalculatorEngine calculatorEngine = new calculatorEngine();//this class handles the calculation
+
+            double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine);
+            double SecondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine);
+            string operation = Console.ReadLine();
         }
     }
 }

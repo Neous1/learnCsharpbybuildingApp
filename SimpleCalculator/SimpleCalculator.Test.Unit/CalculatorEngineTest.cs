07582267a321a9b5dc827498a039e70e4c97b057
Neous1 <Neous1@users.noreply.github.com>
2017-10-17 04:18:03 -0700

    Add unit test for CalculatorEngine and InputConverter

---
+++ SimpleCalculator/SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class CalculatorEngineTest
+    {
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResutlForNonSymbolOperation()
+        {
+            //1. setting upthe data for unit test
+            int number1 = 1;
+                int number2 = 2;
+            double result = _calculatorEngine.Calculate("add", number1, number2);
+
+            //2. process the data thru the method that you want to test
+            //3. compare the result from processing against your assumption
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResutlForSymbolOperation()
+        {
+            //1. setting upthe data for unit test
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("+", number1, number2);
+
+            //2. process the data thru the method that you want to test
+            //3. compare the result from processing against your assumption
+            Assert.AreEqual(3, result);
+        }
+    }
+}
